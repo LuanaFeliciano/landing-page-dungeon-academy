869b128ef87e37e61a6cd77703ee8c02cd830f33
Mateus Escorsi <escorsimateus@gmail.com>
2024-10-29 20:53:03 -0300

    mudando background

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -12,38 +12,40 @@
         </div>
 
         <!-- Título -->
-        <h1 class="q-mt-lg text-h3 text-dark-blue">Dungeon Academy</h1>
+        <h1 class="q-mt-lg text-h3 text-dark">Dungeon Academy</h1>
 
         <!-- Subtítulo Persuasivo -->
-        <h2 class="q-mt-md text-h5 text-dark-grey">Desafie sua mente e vença os monstros!</h2>
+        <h2 class="q-mt-md text-h5 text-dark">
+          Desafie sua mente e vença os monstros!
+        </h2>
 
         <!-- Descrição -->
         <div class="description q-mt-md q-pa-md">
-          <p>
+          <p style="color: black">
             Dungeon Academy é um jogo educativo desenvolvido no GameMaker que
-            proporciona uma experiência de aprendizado única. Ao invés de lutar, você
-            deve responder a perguntas desafiadoras para derrotar monstros e
-            progredir na história. Prepare-se para testar seu conhecimento e
+            proporciona uma experiência de aprendizado única. Ao invés de lutar,
+            você deve responder a perguntas desafiadoras para derrotar monstros
+            e progredir na história. Prepare-se para testar seu conhecimento e
             habilidades!
           </p>
         </div>
 
         <!-- Prova Social -->
-        <div class="social-proof q-mt-md q-pa-md">
+        <div class="text-dark social-proof q-mt-md q-pa-md">
           <h3 class="text-h6 text-dark-blue">O que nossos jogadores dizem:</h3>
           <p class="quote">
             "Dungeon Academy é incrível! Aprendi muito enquanto me divertia!"
-            <br/>- Maria, Estudante
+            <br />- Maria, Estudante
           </p>
           <p class="quote">
             "Um jogo desafiador que realmente estimula o cérebro!"
-            <br/>- João, Professor
+            <br />- João, Professor
           </p>
         </div>
 
         <!-- Benefícios e Proposta de Valor -->
         <div class="benefits q-mt-md q-pa-md">
-          <h3 class="text-h6 text-dark-blue">Benefícios:</h3>
+          <h3 class="text-h6">Benefícios:</h3>
           <ul class="benefit-list">
             <p>Desenvolva suas habilidades de raciocínio lógico.</p>
             <p>Aprimore seu conhecimento em várias áreas.</p>
@@ -63,7 +65,6 @@
       </div>
     </q-page>
   </q-page-container>
-
 </template>
 
 <script setup>
@@ -71,5 +72,3 @@ function goToGithub() {
   window.open('https://github.com/LuanaFeliciano/DungeonAcademy', '_blank');
 }
 </script>
-
-
